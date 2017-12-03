37e782e456be7a6d9561082bb7583f1d38605c0b
Đạt Phan <t Phan@DESKTOP-R9F1HPG>
2017-12-03 21:18:15 +0700

    Add project files.

---
+++ PhanTanDat-b8-b1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PhanTanDat_b8_b1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast,9050);
+            IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.255"), 9050);
+            string hostname = Dns.GetHostName();
+            byte[] data = Encoding.ASCII.GetBytes(hostname);
+            sock.SetSocketOption(SocketOptionLevel.Socket,
+            SocketOptionName.Broadcast, 1);
+            sock.SendTo(data, iep1);
+            sock.SendTo(data, iep2);
+            sock.Close();
+        }
+    }
+}
